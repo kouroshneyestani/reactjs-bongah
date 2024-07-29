799ec3f483ec64e3938c13d0e25a9d4efb0ba749
Kourosh <kouroshsharifineyestani@gmail.com>
2024-07-29 23:03:33 +0330

    chore: integrate Filter and Products components into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
-import ThemeToggle from "./components/ThemeToggle";
-import LanguageSwitcher from "./components/LanguageSwitcher";
+import Filter from "./components/Filter";
+import Products from "./components/Products";
 
 const App = () => {
-    const { t } = useTranslation();
-
     return (
-        <div className="App">
-            <div className="p-4">
-                <LanguageSwitcher />
-                <ThemeToggle />
-            </div>
-            <h1 className="text-2xl">{t("welcome")}</h1>
+        <div>
+            <Filter />
+            <Products />
         </div>
     );
 };
