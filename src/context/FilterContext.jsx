d0c36b613313df11f087788e788b1a211963a026
Kourosh <kouroshsharifineyestani@gmail.com>
2024-07-29 23:01:44 +0330

    feat: add FilterContext for managing filter state

---
+++ src/context/FilterContext.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useState } from "react";
+
+export const FilterContext = createContext();
+
+export const FilterProvider = ({ children }) => {
+    const [filters, setFilters] = useState({
+        priceRange: [0, 1000000],
+        country: "",
+        state: "",
+        propertyType: "",
+        rentOrSell: "",
+    });
+
+    const updateFilters = (newFilters) => {
+        setFilters((prevFilters) => ({ ...prevFilters, ...newFilters }));
+    };
+
+    return (
+        <FilterContext.Provider value={{ filters, updateFilters }}>
+            {children}
+        </FilterContext.Provider>
+    );
+};
