efa7d4f110d5f41aca1f280d11a34c7b3fcb5f2b
Kourosh <kouroshsharifineyestani@gmail.com>
2024-07-29 23:02:08 +0330

    chore: integrate FilterProvider into the root component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { FilterProvider } from "./context/FilterContext";
 import "./i18n";
 import "./index.css";
 
+// Create the root element and render the application
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <FilterProvider>
+            <App />
+        </FilterProvider>
     </React.StrictMode>
 );
